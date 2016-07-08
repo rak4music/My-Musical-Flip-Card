5692291452464c2c5bf11120f9e39e3d6153f493
Hob Spillane <hob.spillane@workday.com>
2016-07-08 10:27:52 -0400

    Made the API more restful by returning a reference to a song instead of all song details on the list request

--- app/controllers/SongsController.scala
+++ app/controllers/SongsController.scala
@@ -2,11 +2,11 @@ package controllers
 
 import javax.inject.Inject
 
-import models.{Phrase, Song, Timing}
-import play.api.libs.json.Json
-import play.api.mvc.{Action, Controller}
 import anorm._
+import models.{Phrase, Song, SongReference, Timing}
 import play.api.db.DBApi
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller, RequestHeader}
 
 class SongsController @Inject() (dbApi: DBApi) extends Controller {
 
@@ -15,6 +15,7 @@ class SongsController @Inject() (dbApi: DBApi) extends Controller {
   implicit val timingWrites = Json.writes[Timing]
   implicit val phraseWrites = Json.writes[Phrase]
   implicit val songWrites = Json.writes[Song]
+  implicit  val songReferenceWrites = Json.writes[SongReference]
 
   def show(id: Int) = Action {
     db.withConnection { implicit c =>
@@ -29,15 +30,19 @@ class SongsController @Inject() (dbApi: DBApi) extends Controller {
     }
   }
 
-  def list() = Action {
+  def list() = Action { implicit request =>
     db.withConnection { implicit c =>
       val songs = SQL("select * from songs")().map { row =>
-        Song(row[Int]("id"), row[String]("title"), row[String]("author"), Timing(row[Int]("timing_upper"), row[Int]("timing_lower")), row[String]("key"), List())
+        rowToSongRefereremce(row)
       }
       Ok(Json.toJson(songs))
     }
   }
   private def rowToSong(row: Row) = {
     Song(row[Int]("id"), row[String]("title"), row[String]("author"), Timing(row[Int]("timing_upper"), row[Int]("timing_lower")), row[String]("key"), List())
+  }
+  private def rowToSongRefereremce(row: Row)(implicit request: RequestHeader) = {
+    val id = row[Int]("id")
+    SongReference(id, row[String]("title"), routes.SongsController.show(id).absoluteURL())
   }
 }
--- app/models/Song.scala
+++ app/models/Song.scala
@@ -1,3 +1,4 @@
 package models
 
 case class Song(val id: Int, val title: String, val author: String, val timing: Timing, val key: String, val phrases: List[Phrase])
+case class SongReference(val id: Int, val title: String, val href: String)
