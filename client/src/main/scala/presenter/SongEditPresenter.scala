d00f3ac6a04d5d5503fe979db72070b1efbb7ac6
Hob Spillane <hob.spillane@workday.com>
2017-05-25 00:08:09 -0400

    Added the add icon & have it floating above the timeline

--- client/src/main/scala/presenter/SongEditPresenter.scala
+++ client/src/main/scala/presenter/SongEditPresenter.scala
@@ -18,13 +18,43 @@ class SongEditPresenter(val song: SongReference, val contentPane: Node) extends 
     val input = dom.document.createElement("input")
     input.setAttribute("id","lyricsInput")
     input.setAttribute("type","text")
-    songDetailNode.appendChild(input)
+    val inputWrapper = dom.document.createElement("div")
+    inputWrapper.setAttribute("id","lyricsInputWrapper")
+    inputWrapper.appendChild(input)
+    createNoteTimeline(songDetailNode)
+    songDetailNode.appendChild(inputWrapper)
   }
 
   def renderDetail(detail: JsSongDetail): Unit = {
     val songDetailNode = dom.document.createElement("div")
-    songDetailNode.setAttribute("id","songDetail")
+    songDetailNode.setAttribute("id","songEditor")
     createSongDetail(songDetailNode, detail)
     contentPane.appendChild(songDetailNode)
   }
+
+  def createNoteTimeline(songDetailNode: Element) = {
+    val line = dom.document.createElement("div")
+    line.setAttribute("id","noteTimeline")
+    val lineContainer = dom.document.createElement("div")
+    lineContainer.setAttribute("id","noteTimelineContainer")
+    lineContainer.innerHTML = "&nbsp;"
+    lineContainer.appendChild(line)
+    songDetailNode.appendChild(lineContainer)
+    val addIcon = dom.document.createElement("img")
+    addIcon.setAttribute("id", "noteTimelineAddIcon")
+    addIcon.setAttribute("src", "/assets/images/add_icon.png")
+    addIcon.classList.add("hidden")
+    songDetailNode.appendChild(addIcon)
+
+    lineContainer.addEventListener("mousemove", (event: dom.MouseEvent) => {
+      addIcon.setAttribute("style","left:" + event.pageX + "px;")
+    })
+    lineContainer.addEventListener("mouseover", (event: dom.MouseEvent) => {
+      addIcon.classList.remove("hidden")
+    })
+    lineContainer.addEventListener("mouseout", (event: dom.MouseEvent) => {
+      addIcon.classList.add("hidden")
+    })
+  }
+
 }
