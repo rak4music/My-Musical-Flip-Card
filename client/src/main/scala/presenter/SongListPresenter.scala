f5cf91c587b11d4117ecd634e0be2c7c8b665995
Rak Dettelback <rak4music@gmail.com>
2017-07-26 19:56:51 -0700

    Made the event handler reachable.

--- client/src/main/scala/presenter/SongListPresenter.scala
+++ client/src/main/scala/presenter/SongListPresenter.scala
@@ -3,6 +3,7 @@ package presenter
 import model.SongReference
 import org.scalajs.dom
 import org.scalajs.dom.Node
+import org.scalajs.dom.raw.Element
 import system.Events.CreateSongEvent
 import system.{EventBus, Events}
 
@@ -10,7 +11,12 @@ import scala.scalajs.js
 
 class SongListPresenter(songs: js.Array[SongReference]) {
   def render(): Node = {
-    val songList = dom.document.createElement("ul")
+    def createSongHandler(createSongNode: Node, songList: Element)(event:CreateSongEvent): Unit={
+      songList.removeChild(createSongNode)
+      //TODO: Replace the nulls below once we're persisting the song
+      songList.appendChild(new SongReferencePresenter(new SongReference(null, event.title, null, true)).render())
+    }
+    val songList: Element = dom.document.createElement("ul")
     songList.setAttribute("id", "songList")
     songList.classList.add("material")
     val addButton = dom.document.createElement("div")
@@ -20,13 +26,11 @@ class SongListPresenter(songs: js.Array[SongReference]) {
     addButton.addEventListener("click", (e: dom.Event) => {
       val createSongPresenter = new CreateSongPresenter()
       val createSongNode = createSongPresenter.render()
-      def createSongHandler(event:CreateSongEvent): Unit={
-        EventBus.removeEventListener(Events.CREATE_SONG, createSongHandler)
-        songList.removeChild(createSongNode)
-        //TODO: Replace the nulls below once we're persisting the song
-        songList.appendChild(new SongReferencePresenter(new SongReference(null, event.title, null, true)).render())
-      }
-      EventBus.addEventListener(Events.CREATE_SONG, createSongHandler)
+      val handler: (CreateSongEvent) => Unit = createSongHandler(createSongNode, songList)
+      EventBus.addEventListener(Events.CREATE_SONG, (event:CreateSongEvent)=>{
+        handler(event)
+        EventBus.removeEventListener(Events.CREATE_SONG, handler)
+      })
       songList.appendChild(createSongNode)
       createSongPresenter setFocus()
     })
